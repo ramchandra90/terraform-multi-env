a1b62b30922d9d3c54c138a9987c25d69adc9795
ramachandra <ramchandra081@gmail.com>
2024-12-23 22:43:37 +1300

    functions

---
+++ workspaces/ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "terraform" {
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = lookup(var.instance_type, terraform.workspace)
+   vpc_security_group_ids = ["sg-0e79b62f544fbce6e"]
+  tags = {
+    Name = "terraform-${terraform.workspace}"
+  }
+}
